680df4869ba64e87103edb14f5f38333f23d3467
amajodev <amajodev@gmail.com>
2024-07-11 07:46:51 +0200

    frontend

---
+++ src/components/UserList.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, List, ListItem, ListItemText } from '@mui/material';
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await axios.get('/api/users');
+            setUsers(response.data);
+        };
+
+        fetchUsers();
+    }, []);
+
+    return (
+        <Container>
+            <List>
+                {users.map(user => (
+                    <ListItem key={user.id}>
+                        <ListItemText primary={user.name} secondary={user.email} />
+                    </ListItem>
+                ))}
+            </List>
+        </Container>
+    );
+};
+
+export default UserList;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import theme from './theme.js';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    ,
   </React.StrictMode>,
 )
